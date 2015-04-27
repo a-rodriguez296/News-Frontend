554ec50ccbdb6e57e9a54e1b89789e1fde42e054
alejo80805 <alejo80805@hotmail.com>
2015-04-27 10:51:35 -0500

    Initial Commit

---
+++ News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by Alejandro Rodriguez on 4/27/15.
+//  Copyright (c) 2015 Alejandro Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
